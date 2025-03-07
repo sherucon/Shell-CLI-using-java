f463860a5359e8ed3cb4d1d5d0ff26bd317c7c0c
Shreyansh Singh <singhshreyansh288@gmail.com>
2025-03-07 13:36:10 +0530

    implementing exit command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,10 @@ public class Main {
             
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-            
+
+            if(input.equals("exit 0")){
+                break;
+            }
             System.out.println(input + ": command not found");
         }
     }
